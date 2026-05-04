3bfd9cbc939bee0b7f79225a2eb03289d092c1b5
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-05-04 16:33:56 +0545

    day1 js

---
+++ javascript/day1/day1syllabus.md
@@ -0,0 +1,28 @@
+🚀 Day 1 — JavaScript Fundamentals (Foundation Day)
+
+1. Introduction to JavaScript
+    - What is JS?
+    - Role of JS in webdev
+
+2. Brief History of JavaScript
+
+3. How JavaScript Works
+
+4. First JavaScript Code
+
+5. Variables
+    - What is variable?
+    - var (variable) - ES5
+    - let (changeable) vs const (can't change able) PI(3.16) - ES6
+    - Name and keyword difference
+
+6. Data Types (Basic)
+    - String
+    - Number
+    - Boolean
+    - others (task search other datatypes) - integer, bigInt
+
+7. Operators
+    - Arithmetic operators (+, -, *, /)
+    - Assignment (=)
+    - Basic comparison (==, ===, >, <)
---
+++ javascript/day1/script.js
@@ -0,0 +1,51 @@
+// console.log("hello everyone")
+// console.log("hello everyone1")
+// console.log("hello everyone2")
+// console.log("hello everyone3")
+
+
+// var email = "xyz@gmail.com"
+
+// var email = "yz@gmail.com"
+
+// let email = "aayush@gmail.com"
+// let email = "aayush@gmail.com"
+
+// const email = "aa@gmail.com"
+
+//Data types
+
+//1. string
+// const a = "rabindra"
+// console.log("a output: ", a);
+
+//2. Number
+// let a = 50;
+// let b = 20;
+
+// console.log(a + b);
+
+//3. Boolean (True or false)
+// const a = true;
+// cosnt b = false;
+
+//OPERATORS
+
+//1. Assignment do multiplication, division, addition, subtraction
+
+//2. Assignment
+// const a = 34;
+
+//3. Comparison operator
+// const a = 34;
+// const b = 20;
+
+const a = "34";
+const b = 20;
+const c = 30;
+
+// console.log("false", a == b);
+console.log(a === b)
+console.log(c > b)
+console.log( c< b)
+console.log(5 === 5)
